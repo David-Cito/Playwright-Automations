154e50db281f00f7158a1e23ce57ba93b8e31672
David-Cito <47098807+David-Cito@users.noreply.github.com>
2025-01-23 08:53:01 -1000

    second workflow

---
+++ Inputs/export.js
@@ -0,0 +1,7 @@
+// Export the variable
+export const myVariable = 42;
+
+// Export a function
+export function myFunction() {
+  console.log("Hello from myFunction!");
+}
---
+++ tests/Testing-2.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+
+test('test', async ({ page }) => {
+
+    await page.goto('https://www.google.com/');
+    await expect(page.getByRole('img', { name: 'Google' })).toBeVisible();
+    
+})
