a85d6dc3a0f56b589df23d6f07558df47e92de4d
David-Cito <47098807+David-Cito@users.noreply.github.com>
2025-01-23 08:59:38 -1000

    3rd workflow

--- tests/Testing-2.spec.ts
+++ tests/Testing-2.spec.ts
@@ -4,6 +4,6 @@ import { test, expect } from '@playwright/test';
 test('test', async ({ page }) => {
 
     await page.goto('https://www.google.com/');
-    await expect(page.getByRole('img', { name: 'Google' })).toBeVisible();
+    await expect(page.getByLabel('Gmail')).toBeVisible();
     
 })
