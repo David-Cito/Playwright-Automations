cbe6a6d473aebacdfc23e7f54424398351062653
David-Cito <47098807+David-Cito@users.noreply.github.com>
2025-02-01 19:47:20 -1000

    .

--- tests/Clock-testing.spec.ts
+++ tests/Clock-testing.spec.ts
@@ -1,32 +1,36 @@
 import { test, expect } from '@playwright/test';
 
+test.use({ storageState: "../Playwright-Automations/auth.json"});
 
 test('test', async ({ browser }) => {
 
-  const context = await browser.newContext()
+  const context = await browser.newContext();
 
-  const pageOne = await context.newPage()
+  const pageOne = await context.newPage();
+
+  //await context.clearCookies();
   /* await pageOne.screenshot({ path: 'screenshot-tab-one.png' })
   await pageTwo.screenshot({ path: 'screenshot-tab-two.png' }) */
 
   // Intuit Account login
-  await pageOne.goto('https://accounts.intuit.com/app/sign-in?app_group=Identity&asset_alias=Intuit.cto.iam.ius');
+/*   await pageOne.goto('https://accounts.intuit.com/app/sign-in?app_group=Identity&asset_alias=Intuit.cto.iam.ius');
   await pageOne.getByTestId('IdentifierFirstIdentifierInput').click();
   await pageOne.getByTestId('IdentifierFirstIdentifierInput').fill('david@koahawaii.com');
   await pageOne.getByTestId('IdentifierFirstSubmitButton').click();
   await pageOne.getByTestId('currentPasswordInput').click();
   await pageOne.getByTestId('currentPasswordInput').fill('Jwy2djwy2d@#!$');
-  await pageOne.getByTestId('passwordVerificationContinueButton').click();
-  const pageTwo = await context.newPage()
-  await pageTwo.goto('https://accounts.intuit.com/app/sign-in?app_group=QBTime&asset_alias=Intuit.qbshared.tsheets&redirect_uri=https%3A%2F%2Ftsheets.intuit.com#w_timecard');
-  await pageTwo.reload();
-  await pageTwo.reload();
+  await pageOne.getByTestId('passwordVerificationContinueButton').click(); */
+  //const pageTwo = await context.newPage();
+  //await pageOne.goto('https://accounts.intuit.com/app/sign-in?app_group=QBTime&asset_alias=Intuit.qbshared.tsheets&redirect_uri=https%3A%2F%2Ftsheets.intuit.com#w_timecard');
+  //await pageTwo.reload();
+  //await pageTwo.reload();
 
   //await pageOne.goto('https://tsheets.intuit.com/#w_timecard');
 
   // Time clock Login
-  /* await pageOne.goto('https://accounts.intuit.com/app/sign-in?app_group=QBTime&asset_alias=Intuit.qbshared.tsheets&redirect_uri=https%3A%2F%2Ftsheets.intuit.com#w_timecard');
-  await pageOne.getByTestId('IdentifierFirstInternationalUserIdInput').click();
+  await pageOne.goto('https://accounts.intuit.com/app/sign-in?app_group=QBTime&asset_alias=Intuit.qbshared.tsheets&redirect_uri=https%3A%2F%2Ftsheets.intuit.com#w_timecard');
+  
+  /* await pageOne.getByTestId('IdentifierFirstInternationalUserIdInput').click();
   await pageOne.getByTestId('IdentifierFirstInternationalUserIdInput').fill('david@koahawaii.com');
   await pageOne.getByTestId('IdentifierFirstSubmitButton').click();
   await pageOne.getByTestId('currentPasswordInput').fill('Jwy2djwy2d@#!$');
@@ -38,7 +42,9 @@ test('test', async ({ browser }) => {
   //Verify Clock out button is visible
   //await expect(pageOne.locator('#timecard_submit')).toContainText('Clock Out');
 
-  await expect(pageTwo.getByRole('button', { name: 'Clock Out' })).toBeVisible({ timeout: 50_000 });
+  await expect(pageOne.getByRole('button', { name: 'Clock Out' })).toBeVisible({ timeout: 50_000 });
+
+  await browser.close();
 
   //await pageTwo.getByRole('button', { name: 'Clock Out' }).click();
 
