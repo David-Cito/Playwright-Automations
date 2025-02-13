a42925507a0ba23ace429f2b8af893d5cce1574c
David-Cito <47098807+David-Cito@users.noreply.github.com>
2025-02-13 06:51:22 -1000

    Update Dash-Input.js

--- Inputs/Dash-Input.js
+++ Inputs/Dash-Input.js
@@ -2,7 +2,7 @@ import { company_address_book, job_titles, New_employee } from '../Test-modules/
 
 
 const Employee_list = [
-  ["David","Stennis","Oahu",job_titles.Assistant_Accountant,'2/24/25'],
+  ["","","",,'2/24/25'],
 ];
 
 /* const Employee_list = [
